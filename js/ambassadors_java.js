9cff9f52dcf8808fdbaca6c633963e4d882b1bfc
nkmwicz <77887856+nkmwicz@users.noreply.github.com>
2021-03-25 00:21:57 -0400

    Added clustering and fixed map orientation.

--- js/ambassadors_java.js
+++ js/ambassadors_java.js
@@ -1,102 +1,41 @@
-var mymap = L.map('mapid').setView([44.95, 21.56], 5);
+var mymap = L.map('mapid').setView([44.95, 17.56], 5);
+var lyrMarkerCluster;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/light-v10',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoibmttd2ljeiIsImEiOiJja2x6ZHdvNGoxaHBpMnZwYnA5YjJpc2U2In0.-uuGSUrEXhzzdIunjYD72w',
-}).addTo(mymap);
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        mapBounds: ([28.96, -12.48], [57.47, 55.59]),
+        minZoom: 3,
+        accessToken: 'pk.eyJ1Ijoibmttd2ljeiIsImEiOiJja2x6ZHdvNGoxaHBpMnZwYnA5YjJpc2U2In0.-uuGSUrEXhzzdIunjYD72w',
+    }).addTo(mymap);
 
-var geojsonLayer = new L.GeoJSON.AJAX('data/diplomats_data.geojson', {pointToLayer: function(feature,latlng){
-    var str = "<p style= text-align:center> "+feature.properties.name +"</p><hr>";
-    str += "<p>Place: "+feature.properties.place +"</p>";
-    str += "<p>Year: "+feature.properties.year +"</p>";
-    str += "<p>Information: "+feature.properties.ambInfo +"</p>";
-// delete the objectID string before publication
-    str += "<p>Object ID: "+feature.properties.objectID +"</p>";
-    return L.circleMarker(latlng).bindPopup(str);
-}});
-geojsonLayer.addTo(mymap);
+    lyrMarkerCluster = L.markerClusterGroup({
+        maxClusterRadius: 20,
+    });
+
+    var geojsonLayer = new L.GeoJSON.AJAX('data/diplomats_data.geojson', {pointToLayer: function(feature,latlng){
+        var str = "<p style= text-align:center> "+feature.properties.name +"</p><hr>";
+        str += "<p>Place: "+feature.properties.place +"</p>";
+        str += "<p>Year: "+feature.properties.year +"</p>";
+        str += "<p>Information: "+feature.properties.ambInfo +"</p>";
+    // delete the objectID string before publication
+        str += "<p>Object ID: "+feature.properties.objectID +"</p>";
+        return L.circleMarker(latlng, {radius: '10'}).bindPopup(str);
+    }});
+    geojsonLayer.addTo(mymap);
+
+    geojsonLayer.on('data:loaded', function(){
+        lyrMarkerCluster.addLayer(geojsonLayer);
+        lyrMarkerCluster.addTo(mymap)
+    });
 
 mymap.scrollWheelZoom.disable()
 
-// This is a clone of above to try and create separeate colored circleMarkers by making new 
-// var geojsonLayer = new L.GeoJSON.AJAX('data/diplomats_data.geojson', {pointToLayer: function(feature,latlng){
-//     var str = "<p style= text-align:center> "+feature.properties.name +"</p><hr>";
-//     str += "<p>Place: "+feature.properties.place +"</p>";
-//     str += "<p>Year: "+feature.properties.year +"</p>";
-//     str += "<p>Information: "+feature.properties.ambInfo +"</p>";
-//     str += "<p>Object ID: "+feature.properties.objectID +"</p>";
-//     geojsonLayer.then(function(data) {
-//         var ottomanempire = L.geoJson(data);
-//                 var holyromanempire = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.place == [ "Ottoman Empire" ];
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-//                     icon: cafeIcon
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//         });
-//         var others = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.BusType != "Cafe";
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//     return L.circleMarker(latlng).bindPopup(str);
-// }});
-// geojsonLayer.addTo(mymap);
-
-// // adding new information and mouseover and filters to above geojsonLayer
-// var geojsonLayer = new L.GeoJSON.AJAX('data/ambassadors_data.geojson');
-// geojsonLayer.then(function(data){
-//     var allbusinesses = L.geoJson(data);
-//     var cafes = L.geoJson(data, {
-//     filter: function(feature, layer) {
-//         return feature.properties.BusType == "Cafe";
-// },
-// pointToLayer: function(feature, latlng) {
-//     return L.marker(latlng, {
-//         icon: cafeIcon
-//     }).on('mouseover', function() {
-//         this.bindPopup(feature.properties.Name).openPopup();
-//     });
-// }
-// });
-// })
-
-// {pointToLayer: function(feature,latlng){
-//     var str = "<p> "+feature.properties.Place +"</p><hr>";
-//     str += "<p>Place: "+feature.properties.Place +"</p>";
-//     str += "<p>Year: "+feature.properties.Year +"</p>";
-//     return L.circleMarker(latlng).bindPopup(str);
-// }});
-// geojsonLayer.addTo(mymap);
-
-// var markers = L.markerClusterGroup();
-// geojsonLayer.feature.forEach(function(){
-//     markers.addLayer(L.circleMarker(latlng));
-// })
-// mymap.addLayer(markers);
-
-// trying to make popup of Place on mouseover
-// circleMarker.on('mouseover', function(e){
-//     var str = "<p> "+feature.properties.Place +"</p>";
-//     e.openPopup(str);
-// });
-
 $("#back-button").click(function(){
-    mymap.setView([44.95, 21.56], 5);
+    mymap.setView([44.95, 17.56], 5);
     mymap.closePopup();
 });
 
