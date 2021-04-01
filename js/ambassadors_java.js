f573e07404495449993b4304c469863618f85247
Nathan Michalewicz <nathan@mwicz.com>
2021-03-31 20:55:43 -0400

    Working on creating layers based on dates

--- js/ambassadors_java.js
+++ js/ambassadors_java.js
@@ -1,52 +1,157 @@
 var mymap = L.map('mapid').setView([44.95, 13.56], 5);
-var lyrMarkerCluster;
+var lyrAllDatesCluster;
+var lyrAllDates;
+var lyr1515To1525;
+var lyr1515To1525Cluster;
+var lyrEsri_WorldShadedRelief;
+var circleMarker;
+var circleMarker1;
+var ctlLayers;
+var objBasemap;
+var objOverlays;
+var mrkCircles;
 
- L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/light-v10',
-        tileSize: 512,
-        zoomOffset: -1,
-        mapBounds: ([28.96, -12.48], [57.47, 55.59]),
-        minZoom: 3,
-        accessToken: 'pk.eyJ1Ijoibmttd2ljeiIsImEiOiJja2x6ZHdvNGoxaHBpMnZwYnA5YjJpc2U2In0.-uuGSUrEXhzzdIunjYD72w',
+$(document).ready(function(){
+    var lyrEsri_WorldShadedRelief = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
+	maxZoom: 13
     }).addTo(mymap);
 
-    lyrMarkerCluster = L.markerClusterGroup({
-        maxClusterRadius: 5,
-    });
+    lyrAllDatesCluster = L.markerClusterGroup({
+        maxClusterRadius: 1,
+        });
 
-    var geojsonLayer = new L.GeoJSON.AJAX('data/diplomats_data.geojson', {pointToLayer: function(feature,latlng){
+    // loading All dates data to the map
+    var lyrAllDates = new L.GeoJSON.AJAX('data/diplomats_data.geojson',{pointToLayer: function(feature,latlng){
         var str = "<p style= text-align:center> "+feature.properties.name +"</p><hr>";
-        str += "<p>Place: "+feature.properties.place +"</p>";
-        str += "<p>Year: "+feature.properties.year +"</p>";
-        str += "<p>Information: "+feature.properties.ambInfo +"</p>";
+            str += "<p>Place: "+feature.properties.place +"</p>";
+            str += "<p>Year: "+feature.properties.year +"</p>";
+            str += "<p>Information: "+feature.properties.ambInfo +"</p>";
     // delete the objectID string before publication
-        str += "<p>Object ID: "+feature.properties.objectID +"</p>";
-        return L.circleMarker(latlng, {radius: '10'}).bindPopup(str);
-    }});
-    geojsonLayer.addTo(mymap);
+            str += "<p>Object ID: "+feature.properties.objectID +"</p>";
+            if (feature.properties.place == 'Ottoman Empire'){
+                fillCircle='red'
+            } else {
+                fillCircle='green'
+            };
+            var circleMarker = L.circleMarker(latlng, {radius: '10', fillColor:fillCircle})
+            .on('mouseover',function(){this.bindPopup(str).openPopup()})
+            .on('mouseout', function(){this.closePopup()});
+            return circleMarker;
+        }
+    });
+    mymap.addLayer(lyrAllDates);
 
-    geojsonLayer.on('data:loaded', function(){
-        lyrMarkerCluster.addLayer(geojsonLayer);
-        lyrMarkerCluster.addTo(mymap)
+    lyr1515To1525Cluster = L.markerClusterGroup({
+        maxClusterRadius: 5,
+        });
+
+    var lyr1515To1525 = new L.GeoJSON.AJAX('data/diplomats_data.geojson',
+        {filter: function(feature, layer){
+            return feature.properties.year>="1515" && feature.properties.year<="1525"
+        },
+        pointToLayer: function(feature,latlng){
+            var str = "<p style= text-align:center> "+feature.properties.name +"</p><hr>";
+            str += "<p>Place: "+feature.properties.place +"</p>";
+            str += "<p>Year: "+feature.properties.year +"</p>";
+            str += "<p>Information: "+feature.properties.ambInfo +"</p>";
+    // delete the objectID string before publication
+            str += "<p>Object ID: "+feature.properties.objectID +"</p>";
+            if (feature.properties.place == 'Ottoman Empire'){
+                fillCircle='red'
+                } else {
+                fillCircle='green'
+            };
+            var circleMarker1 = L.circleMarker(latlng, {radius: '10', fillColor:fillCircle})
+                .on('mouseover',function(){this.bindPopup(str).openPopup()})
+                .on('mouseout', function(){this.closePopup()});
+                return circleMarker1;
+        }
     });
 
-mymap.scrollWheelZoom.disable()
 
-$("#back-button").click(function(){
-    mymap.setView([44.95, 13.56], 5);
-    mymap.closePopup();
-});
+    // function returnMarkers(json, latlng){
+    //     var att = json.properties;
+    //     if (att.place == 'Ottoman Empire'){
+    //         var clrCircles = 'blue';
+    //         var fillCircles = 'blue';
+    //     } else {
+    //         var clrCircles = 'red';
+    //         var fillCircles =  'red';
+    //     };
+    //     var str = "<p style= text-align:center> "+att.name +"</p><hr>";
+    //         str += "<p>Place: "+att.place +"</p>";
+    //         str += "<p>Year: "+att.year +"</p>";
+    //         str += "<p>Information: "+att.ambInfo +"</p>";
+    // // delete the objectID string before publication
+    //         str += "<p>Object ID: "+att.objectID +"</p>";
+    //     var mrkCircles = L.circleMarker(latlng,{radius:10, color:clrCircles, fillColor:fillCircles}).bindTooltip(str);
+    //     return mrkCircles;
+    // };
 
-//Zooms to Paris, France on click text-button "zoomto"
-$("#zoomto").click(function(){
-    mymap.setView([48.85, 2.35], 7)
+    // mrkCircles.bindTooltip('Hi');
+
+    // creating the Layer control
+    objBasemap = {
+        "Esri Shaded Relief": lyrEsri_WorldShadedRelief,
+    };
+
+    objOverlays = {
+        "1515-1600": lyrAllDates,  
+    };
+
+    ctlLayers = L.control.layers(objBasemap, objOverlays).addTo(mymap);
+
+        // clustering--trying to make it show (feature.properties.place) on hover.
     
-});
+    // lyrAllDatesCluster.addLayer(circleMarker);
+    // mymap.addLayer(lyrAllDatesCluster);
+    
+    
+    lyrAllDates.on('data:loaded', function(){
+        lyrAllDatesCluster.addLayer(lyrAllDates);
+        lyrAllDatesCluster.addLayer(lyr1515To1525);
+        });
+    mymap.addLayer(lyrAllDatesCluster);
 
-//Shows coordinates of mouse in "map_coords" section.
-mymap.on('mousemove', function(e){
+    // lyr1515To1525.on('data:loaded', function(){
+    //     lyr1515To1525Cluster.removeLayer(lyrAllDatesCluster);
+    //     lyr1515To1525Cluster.addLayer(lyr1515To1525Cluster);
+    //     // lyr1515To1525Cluster.addTo(mymap);            
+    //                 // .bindpopup(feature.properties.place).openPopup())
+    //     });
+        
+
+    // lyrAllDatesCluster.on('clustermouseover', function(){
+    //     this.bindPopup(json.properties.place).openPopup()});
+    // lyrAllDatesCluster.on('clusterclick', (){
+    //     this.spiderfy()});
+
+    mymap.scrollWheelZoom.disable()
+
+    // ************Action Buttions**************
+    $("#back-button").click(function(){
+        mymap.setView([44.95, 13.56], 5);
+        mymap.closePopup();
+        mymap.addLayer(lyrAllDates);
+        mymap.refreshCluster(lyrAllDates);
+        mymap.removeLayer(lyr1515To1525);
+    });
+
+    //Zooms to Paris, France on click text-button "zoomto"
+    $("#zoomto").click(function(){
+        mymap.removeLayer(lyrAllDates);
+        // mymap.addLayer(lyr1515To1525);
+        mymap.addLayer(lyr1515To1525);
+        mymap.addLayer(lyrAllDatesCluster);
+        lyrAllDatesCluster.refreshCluster(lyrAllDates);
+    });
+
+    //********Shows coordinates of mouse in "map_coords" section******
+    mymap.on('mousemove', function(e){
+        var str = "Latitude: "+e.latlng.lat.toFixed(2)+" Longitude: "+e.latlng.lng.toFixed(2)+" Zoom level: "+mymap.getZoom(); 
     var str = "Latitude: "+e.latlng.lat.toFixed(2)+" Longitude: "+e.latlng.lng.toFixed(2)+" Zoom level: "+mymap.getZoom(); 
-    $("#map_coords").html(str);
-});
+        var str = "Latitude: "+e.latlng.lat.toFixed(2)+" Longitude: "+e.latlng.lng.toFixed(2)+" Zoom level: "+mymap.getZoom(); 
+        $("#map_coords").html(str);
+    });
+})
