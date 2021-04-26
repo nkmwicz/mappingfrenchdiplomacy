c76391c56576b666b4d3b2e0a2b26fda30272605
nkmwicz <77887856+nkmwicz@users.noreply.github.com>
2021-04-26 16:54:15 -0400

    styled cluster colors by place.

--- js/sixteenthcenturydiplomats_java.js
+++ js/sixteenthcenturydiplomats_java.js
@@ -27,6 +27,14 @@ var objOverlays;
 var mrkCircles;
 var slider;
 var mapdates;
+var clusterWidth;
+var clusterColor;
+var clusterBorder;
+var clusterTextColor;
+var clusterMaxWidth;
+var marginTop;
+var marginTop2;
+var marginLeft;
 
 $(document).ready(function(){
     var lyrEsri_WorldShadedRelief = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
@@ -44,11 +52,76 @@ $(document).ready(function(){
         // console.log(json)
 
         clusters = L.markerClusterGroup.layerSupport({
+            iconCreateFunction: function(cluster) {
+                clusterWidth = 30 +((cluster.getChildCount()))
+                if (cluster._cLatLng.lat == '55.68', cluster._cLatLng.lng == '12.57'){
+                    clusterColor = "rgba(193,184,78,0.8)";
+                } else if (cluster._cLatLng.lat == '51.51', cluster._cLatLng.lng == '-0.12'){
+                    clusterColor = "rgba(20,234,52,0.8)"
+                } else if (cluster._cLatLng.lat == '44.84', cluster._cLatLng.lng == '11.62'){
+                    clusterColor = "rgba(254,191,229.0.8)"
+                } else if (cluster._cLatLng.lat == '46.21', cluster._cLatLng.lng == '6.14'){
+                    clusterColor = "rgba(194,243,253,0.8)"
+                } else if (cluster._cLatLng.lat == '46.66', cluster._cLatLng.lng == '9.63'){
+                    clusterColor = "rgba(230,0,0,0.8)"
+                } else if (cluster._cLatLng.lat == '48.21', cluster._cLatLng.lng == '16.36'){
+                    clusterColor = "rgba(255,255,0,0.8)"
+                } else if (cluster._cLatLng.lat == '52.37', cluster._cLatLng.lng == '4.89'){
+                    clusterColor = "rgba(180,253,223,0.8)"
+                } else if (cluster._cLatLng.lat == '41.01', cluster._cLatLng.lng == '28.96'){
+                    clusterColor = "rgba(0,0,0,0.8)"
+                } else if (cluster._cLatLng.lat == '52.23', cluster._cLatLng.lng == '21.02'){
+                    clusterColor = "rgba(214,216,255,0.8)"
+                } else if (cluster._cLatLng.lat == '38.72', cluster._cLatLng.lng == '-9.13'){
+                    clusterColor = "rgba(255,253,180,0.8)"
+                } else if (cluster._cLatLng.lat == '41.89', cluster._cLatLng.lng == '12.51'){
+                    clusterColor = "rgba(239,202,253,0.8)"
+                } else if (cluster._cLatLng.lat == '45.06', cluster._cLatLng.lng == '7.68'){
+                    clusterColor = "rgba(226,253,211,0.8)"
+                } else if (cluster._cLatLng.lat == '51.05', cluster._cLatLng.lng == '13.35'){
+                    clusterColor = "rgba(253,217,198,0.8)"
+                } else if (cluster._cLatLng.lat == '55.95', cluster._cLatLng.lng == '-3.19'){
+                    clusterColor = "rgba(255,255,190,0.8)"
+                } else if (cluster._cLatLng.lat == '40.43', cluster._cLatLng.lng == '-3.7'){
+                    clusterColor = "rgba(234,234,234,0.8)"
+                } else if (cluster._cLatLng.lat == '46.94', cluster._cLatLng.lng == '7.45'){
+                    clusterColor = "rgba(184,0,230,0.8)"
+                } else if (cluster._cLatLng.lat == '43.46', cluster._cLatLng.lng == '11.14'){
+                    clusterColor = "rgba(187,223,254,0.8)"
+                } else if (cluster._cLatLng.lat == '45.44', cluster._cLatLng.lng == '12.33'){
+                    clusterColor = "rgba(156,156,156,0.8)"
+                } else {
+                    clusterColor = "white"
+                };
+                if (cluster._cLatLng.lat == '41.01', cluster._cLatLng.lng == '28.96'){
+                    clusterTextColor = "white",
+                    clusterBorder = "solid white"
+                } else {
+                    clusterTextColor = "black",
+                    clusterBorder = "solid black"
+                };
+                var marginTop = (clusterWidth-23)/2;
+                var marginLeft = (clusterWidth-10)/2;
+                var marginTop2 = (clusterWidth-15)/2;
+                return L.divIcon({ 
+                    html: '<div class="clusterdiv" style="width:'+clusterWidth+'px; height: '+clusterWidth+'px; max-height:'+clusterMaxWidth+'px; max-width:'+clusterMaxWidth+'px; margin-top:-'+marginTop2+'px; margin-left:-'+marginLeft+'px; background-color:'+clusterColor+'; color:'+clusterTextColor+'; border:'+clusterBorder+'"><div class="clustertext" style="margin-top:'+marginTop+'px"><b>' + cluster.getChildCount() + '</b></div></div>',
+                    // className: 'clusterdiv',
+                    // iconSize: new L.Point(width, width) 
+                });
+                console.log(cluster);
+            },
             maxClusterRadius: 0,
         });
+
+        
+        clusters.on('clusterclick', function (a) {
+            // a.layer is actually a cluster
+            console.log('cluster ' + a.layer.getAllChildMarkers());
+        });
+
         clusters.on('clustermouseover', function(a){
             if (a.layer._cLatLng.lat == '55.68', a.layer._cLatLng.lng == '12.57'){
-                popupText = "<p>Denmark</p>"
+                popupText = "<p>Denmark</>"
             } else if (a.layer._cLatLng.lat == '51.51', a.layer._cLatLng.lng == '-0.12'){
                     popupText = "<p>England</p>"
             } else if (a.layer._cLatLng.lat == '44.84', a.layer._cLatLng.lng == '11.62'){
@@ -178,9 +251,10 @@ $(document).ready(function(){
         //     console.log(e)
         // });
 
-        clusters.on('clusterclick', function(e){
-            console.log(e);
-        });
+        // clusters.on('clusterclick', function(e){
+        //     console.log(e);
+        // });
+        
 
         // mymap.on('click', function(e){
         //     console.log(e);
@@ -241,13 +315,26 @@ $(document).ready(function(){
         // *****Event Buttons*****
         $(".example").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1545,1555]);
             mymap.fitBounds(clusters.getBounds());
             document.getElementById("map-title").innerHTML = "Example Event, 1545-1555"
         });
 
+        $(".homeview").click(function(){
+            mymap.fitBounds(lyrAllDates.getBounds());
+            mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
+            slider.noUiSlider.set([1515,1600]);
+            mymap.fitBounds(clusters.getBounds());
+        });
+
         $(".poland1570s").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1570,1580]);
             lyrAllDates.eachLayer(function(layer){
                 if (layer.feature.properties.place=="Poland"){
@@ -260,34 +347,34 @@ $(document).ready(function(){
             mymap.setView([52.21, 21.01]);
         });
 
-        $(".homeview").click(function(){
-            mymap.fitBounds(lyrAllDates.getBounds());
-            mymap.closePopup();
-            slider.noUiSlider.set([1515,1600]);
-            mymap.fitBounds(clusters.getBounds());
-        });
-
         $(".1516").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1516, 1516]);
             mymap.fitBounds(clusters.getBounds(), {padding:[100,100]});
         });
 
         $(".1547").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1547, 1547]);
             mymap.fitBounds(clusters.getBounds());
         });
 
         $(".1515-1520").click(function(){
             mymap.closePopup();
-            mymap.fitBounds(lyrAllDates.getBounds());
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1515, 1520]);
             mymap.fitBounds(clusters.getBounds(), {padding:[50,50]});
         });
 
         $(".Bonnivet1518").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1518, 1518]);
             lyrAllDates.eachLayer(function(layer){
                 if (layer.feature.properties.objectID=="859"){
@@ -301,6 +388,8 @@ $(document).ready(function(){
 
         $(".HRE1519").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1519, 1519]);
             lyrAllDates.eachLayer(function(layer){
                 if (layer.feature.properties.place=="Holy Roman Empire"&&layer.feature.properties.year=="1519"){
@@ -314,6 +403,8 @@ $(document).ready(function(){
 
         $(".daugerant1520-1525").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1520, 1525]);
             lyrAllDates.eachLayer(function(layer){
                 if (layer.feature.properties.place=="Swiss"&&layer.feature.properties.year>="1520"&&layer.feature.properties.year<="1525"){
@@ -328,6 +419,8 @@ $(document).ready(function(){
 
         $(".portugal1520-1525").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1520, 1525]);
             lyrAllDates.eachLayer(function(layer){
                 if (layer.feature.properties.place=="Portugal"&&layer.feature.properties.year>="1520"&&layer.feature.properties.year<="1525"){
@@ -342,59 +435,79 @@ $(document).ready(function(){
 
         $(".1520-1525").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1520, 1525]);
             mymap.fitBounds(clusters.getBounds(), {padding:[50,50]});
         });
 
         $(".1515-1525").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1515, 1525]);
             mymap.fitBounds(clusters.getBounds(), {padding:[50,50]});
         });
 
         $(".1525-1535").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1525, 1535]);
             mymap.fitBounds(clusters.getBounds(), {padding:[50,50]});
         });
 
         $(".1515-1535").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1515, 1535]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });
 
         $(".1535-1555").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1535, 1555]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });
 
         $(".1535-1540").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1535, 1540]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });
         
         $(".1540-1545").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1540, 1545]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });  
 
         $(".1545-1550").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1545, 1550]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });  
         $(".1550-1555").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1550, 1555]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });  
 
         $(".1559-1600").click(function(){
             mymap.closePopup();
+            lyrGroup.clearLayers();
+            lyrGroup.addLayer(lyrAllDates);
             slider.noUiSlider.set([1559, 1600]);
             mymap.fitBounds(clusters.getBounds(), {padding:[150,150]});
         });  
