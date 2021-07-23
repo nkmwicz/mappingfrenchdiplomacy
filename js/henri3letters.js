5833f255e70c60fe192291c2bb535af511f7a54e
Nathan <nathan@mwicz.com>
2021-07-22 21:17:55 -0400

    package updates fixed blank space problem.

--- js/henri3letters.js
+++ js/henri3letters.js
@@ -358,7 +358,6 @@ fetch('data/henri3letters.geojson', {
   });
 
 
-
   // *****Engage slider and search filter together with data*****
   slider.noUiSlider.on('set', function(e) {
     filters.range =
